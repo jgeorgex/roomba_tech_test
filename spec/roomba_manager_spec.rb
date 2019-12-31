6d4d50c2f64effeb2841dbd9e008e40808681932
Julian George <jules104@gmail.com>
2019-12-31 13:51:24 +0000

    Test refactored to pass for complete input

--- spec/roomba_manager_spec.rb
+++ spec/roomba_manager_spec.rb
@@ -2,7 +2,7 @@ require 'roomba_manager'
 
 describe '#get_input' do
     it 'gets the input of the text file' do
-        expect(get_input).to eq('5 5')
+        expect(get_input).to include('5 5')
     end
 end
 

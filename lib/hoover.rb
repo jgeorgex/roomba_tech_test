e202b8bde94d094ff00d4fda2bb8a872922e6819
Julian George <jules104@gmail.com>
2020-01-05 13:53:25 +0000

    Add get_start_coordinates method to Hoover class

--- lib/hoover.rb
+++ lib/hoover.rb
@@ -1,9 +1,9 @@
 class Hoover
 
-    hoover_route = []
+    @hoover_route = []
     # [[1,2],[1,3],[1,4],[2,4],[2,3],[3,3],[4,3],[4,2],[3,2],[3,3],[2,3],[1,3]]
 
     def get_start_coordinates
-        [1,2]
+        RoombaManager.new.get_start_position
     end
 end

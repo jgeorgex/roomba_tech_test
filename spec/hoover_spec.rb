5355a5ac8f406ed2898f3c8b508889420c4e9a60
Julian George <jules104@gmail.com>
2020-01-05 13:23:49 +0000

    Replace route method with get_start_coordiates method

--- spec/hoover_spec.rb
+++ spec/hoover_spec.rb
@@ -1,10 +1,10 @@
 require 'hoover'
 
 describe Hoover do
-    describe '#hoover_route' do
-        it 'returns coordinates of route hoover takes' do
+    describe '#get_start_coordinates' do
+        it 'Adds coordinates of hoover start position to route' do
             hoover = Hoover.new
-            expect(hoover.hoover_route).to eq([[1,2],[1,3],[1,4],[2,4],[2,3],[3,3],[4,3],[4,2],[3,2],[3,3],[2,3],[1,3]])
+            expect(hoover.get_start_coordinates).to eq([1,2])
         end
     end
 end

5bb88e5b92d7d5ad79ad7c8a6702eed20483910c
Julian George <jules104@gmail.com>
2020-01-05 14:22:08 +0000

    Add initialize method to hoover and hoover_roue to store hoover route

--- lib/hoover.rb
+++ lib/hoover.rb
@@ -1,9 +1,13 @@
 class Hoover
 
+    def initialize
     @hoover_route = []
     # [[1,2],[1,3],[1,4],[2,4],[2,3],[3,3],[4,3],[4,2],[3,2],[3,3],[2,3],[1,3]]
+    end
 
     def get_start_coordinates
-        RoombaManager.new.get_start_position
+        start_position = RoombaManager.new.get_start_position
+        @hoover_route << start_position
+        start_position
     end
 end

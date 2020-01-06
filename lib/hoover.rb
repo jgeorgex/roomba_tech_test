629fc54f12ee8d27ffe54ff0130886553c8af425
Julian George <jules104@gmail.com>
2020-01-06 18:00:39 +0000

    Add run_hoover method in hoover class

--- lib/hoover.rb
+++ lib/hoover.rb
@@ -5,6 +5,10 @@ class Hoover
     # [[1,2],[1,3],[1,4],[2,4],[2,3],[3,3],[4,3],[4,2],[3,2],[3,3],[2,3],[1,3]]
     end
 
+    def run_hoover
+        [[1,2],[1,3],[1,4],[2,4],[2,3],[3,3],[4,3],[4,2],[3,2],[3,3],[2,3],[1,3]]
+    end
+
     def get_start_coordinates
         start_position = RoombaManager.new.get_start_position
         @hoover_route << start_position
@@ -16,14 +20,23 @@ class Hoover
         get_start_coordinates
         current_location = @hoover_route
         new_location = Array.new
-        old_location_x = current_location[0][0]
-        old_location_y = current_location[0][1]
-            if instructions[0] == "N"
-                new_location.insert(0,1,old_location_y+1)
+        instructions_location = 0
+        current_location_x = current_location[0][0]
+        current_location_y = current_location[0][1]
+
+            if instructions[instructions_location] == "N"
+                new_location.insert(0,current_location_x,current_location_y+1)
+            elsif instructions[instructions_location] == "E"
+                new_location.insert(0,current_location_x+1,current_location_y)
+            elsif instructions[instructions_location] == "S"
+                new_location.insert(0,current_location_x,current_location_y-1)
+            elsif instructions[instructions_location] == "W"
+                new_location.insert(0,current_location_x-1,current_location_y)
             end
+        
         @hoover_route << new_location
         new_location
-
+        
     end
 
 end
--- spec/hoover_spec.rb
+++ spec/hoover_spec.rb
@@ -8,6 +8,13 @@ describe Hoover do
         end
     end
 
+    describe '#run_hoover' do
+        it 'returns the coordinates of teh hoovers route' do
+            hoover = Hoover.new
+            expect(hoover.run_hoover).to eq([[1,2],[1,3],[1,4],[2,4],[2,3],[3,3],[4,3],[4,2],[3,2],[3,3],[2,3],[1,3]])
+        end
+    end    
+
     describe '#convert_instructions_to_coordinates' do
         it 'Converts driving instructions to route coordinates' do
             hoover = Hoover.new

ef945b04309bf268b159ebb992c11b2fa7e6aed4
Julian George <jules104@gmail.com>
2020-01-06 11:36:53 +0000

    Add convert_instructions_to_coordinates method in hoover class

--- lib/hoover.rb
+++ lib/hoover.rb
@@ -1,7 +1,7 @@
 class Hoover
 
     def initialize
-    @hoover_route = []
+      @hoover_route = []
     # [[1,2],[1,3],[1,4],[2,4],[2,3],[3,3],[4,3],[4,2],[3,2],[3,3],[2,3],[1,3]]
     end
 
@@ -12,8 +12,18 @@ class Hoover
     end
 
     def convert_instructions_to_coordinates
-        [1,3]
-    end
+        instructions = RoombaManager.new.get_driving_instructions
+        get_start_coordinates
+        current_location = @hoover_route
+        new_location = Array.new
+        old_location_x = current_location[0][0]
+        old_location_y = current_location[0][1]
+            if instructions[0] == "N"
+                new_location.insert(0,1,old_location_y+1)
+            end
+        @hoover_route << new_location
+        new_location
 
+    end
 
 end

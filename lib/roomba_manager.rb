c700e48ec6576a2cbeeadc17146165b83d0132e9
Julian George <jules104@gmail.com>
2020-01-07 18:44:43 +0000

    Refactor RoombaManager methods and test corrected for simpler input file and unnecessary methods deleted

--- lib/roomba_manager.rb
+++ lib/roomba_manager.rb
@@ -8,14 +8,6 @@ def get_room_dimensions
   room_dimensions = get_input[0].chop.split(" ").map(&:to_i)
 end
 
-def get_room_dimension_x
-  get_room_dimensions[0]
-end
-
-def get_room_dimension_y
-  get_room_dimensions[1]
-end
-
 def get_start_position
   start_position = get_input[1].chop.split(" ").map(&:to_i)
 
@@ -26,13 +18,7 @@ def get_driving_instructions
 end
 
 def get_dirt_patches
-  dirt_patches = []
-  dirt_patches_counter = 2
-  dirt_patches_total = get_input.length-3
-  while dirt_patches_counter <= 4 do
-  dirt_patches << get_input[dirt_patches_counter].chop.split(" ").map(&:to_i)
-  dirt_patches_counter+=1
-  end
+  dirt_patches = get_input[2].chop.split(" ").map(&:to_i)
   return dirt_patches
 end
 end
--- spec/roomba_manager_spec.rb
+++ spec/roomba_manager_spec.rb
@@ -5,7 +5,7 @@ describe RoombaManager do
 describe '#get_input' do
     it 'gets the input of the text file' do
         roomba_manager = RoombaManager.new
-        expect(roomba_manager.get_input).to include('NNESEESWNWW')
+        expect(roomba_manager.get_input).to include('EW')
     end
 end
 
@@ -16,38 +16,24 @@ describe '#get_room_dimensions' do
     end
 end
 
-describe '#get_room_dimension_x' do
-    it 'gets 5 from the input text file' do
-        roomba_manager = RoombaManager.new
-        expect(roomba_manager.get_room_dimension_x).to eq(5)
-    end
-end
-
-describe '#get_room_dimension_y' do
-    it 'gets 5 from the input text file' do
-        roomba_manager = RoombaManager.new
-        expect(roomba_manager.get_room_dimension_y).to eq(5)
-    end
-end
-
 describe '#get_start_position' do
-    it 'gets [1,2] from the input text file' do
+    it 'gets [0,0] from the input text file' do
         roomba_manager = RoombaManager.new
-        expect(roomba_manager.get_start_position).to eq([1,2])
+        expect(roomba_manager.get_start_position).to eq([0,0])
     end
 end
 
 describe '#get_driving_instructions' do
-    it 'gets NNESEESWNWW from the input file' do
+    it 'gets EW from the input file' do
         roomba_manager = RoombaManager.new
-        expect(roomba_manager.get_driving_instructions).to eq("NNESEESWNWW")
+        expect(roomba_manager.get_driving_instructions).to eq("EW")
     end
 end
 
 describe '#get_dirt_patches' do 
-    it 'gets [1,0],[2,2],[2,3] from the input file' do
+    it 'gets [1,0] from the input file' do
         roomba_manager = RoombaManager.new
-        expect(roomba_manager.get_dirt_patches).to eq([[1,0],[2,2],[2,3]])
+        expect(roomba_manager.get_dirt_patches).to eq([1,0])
     end
 end
 end

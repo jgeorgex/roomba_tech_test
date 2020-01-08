60602d482fcf643de0cf70f38207cd8cb4477369
Julian George <jules104@gmail.com>
2020-01-08 09:39:50 +0000

    Refactor get_final_position method to use calculate_route method for input in hoover class

--- lib/hoover.rb
+++ lib/hoover.rb
@@ -1,16 +1,16 @@
 class Hoover
 
-def run_hoover
-    "The final poition is #{get_final_position} and 1 piece of dirt was cleaned up"
-end
+    def run_hoover
+        "The final poition is #{get_final_position} and 1 piece of dirt was cleaned up"
+    end
 
-def get_final_position
-    [0,0].to_s
-end
+    def get_final_position
+        calculate_route.last.to_s
+    end
 
-def calculate_route
-    [[0,0],[1,0],[0,0]]
-end
+    def calculate_route
+        [[0,0],[1,0],[0,0]]
+    end
 
 
 end
